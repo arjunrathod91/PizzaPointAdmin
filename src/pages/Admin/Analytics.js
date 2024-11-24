a43190c86817d677e91f5f0eea30dea534af5f24
ArjunRathod <ar885209@gmail.com>
2024-11-24 11:43:22 +0530

    adding new features to admin

--- src/pages/Admin/Analytics.js
+++ src/pages/Admin/Analytics.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "../../context/Context";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { useMediaQuery } from "@mui/material";
 
 function Analytics() {
+  const { allorders, setAllOrders, rightSec, setRIghtSec } =
+    useContext(Context);
+    const isMobile = useMediaQuery("(max-width:600px)");
   return (
     <div className="analytics">
+       {isMobile ? (
+        <div onClick={() => setRIghtSec(false)}>
+          <ArrowBackIcon />
+        </div>
+      ) : (
+        ""
+      )}
       <h2>Analytics</h2>
       <div className="content-box">
         <div>Sales</div>
